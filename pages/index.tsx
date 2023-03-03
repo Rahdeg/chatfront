2155a4ee7b2af64be7c9b54d380e81831d983636
Rahdeg <walett95@gmail.com>
2023-03-03 08:14:50 +0100

    completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,34 @@
+import axios from 'axios'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
+
 
 const Home: NextPage = () => {
+  const [prompt, setPrompt] = useState('')
+  const [response, setResponse] = useState < string | null> (null);
+  const handleClick = async ()=>{
+    await axios.post('http://localhost:8080/api/v1/chat',{prompt})
+    .then((res)=>{setResponse(res.data)})
+    .catch((err)=>{console.log(err)})
+  };
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
+   <div className=' w-screen h-screen flex items-center justify-center p-24'>
+    <Head>
+        <title>Chat Us</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+    <div className='w-full py-6 bg-[#1b2439] flex items-center justify-center  rounded-lg shadow-sm gap-6 flex-col '>
+      <div className=' w-4/5 py-2   rounded-lg bg-[#232e47] flex items-center justify-center gap-1 shadow-md '>
+        <input className='w-full h-full bg-transparent text-[#fbfbfb] font-semibold border-none outline-none ml-3' type='text' title='text' placeholder='Ask Anything to Ai....!'  onChange={(e)=>setPrompt(e.target.value)} value={prompt}/>
+        <button className='border-none outline-none py-3 w-28 font-medium bg-[#161d2f] text-[#fbfbfb] rounded-md cursor-pointer transition ease-in-out mr-3 active:transform' onClick={handleClick}>Ask AI</button>
+      </div>
+      <div className='w-4/5 py-2 h-44 animate-pulse   rounded-lg bg-[#232e47] flex items-center justify-center gap-1 shadow-md '>
+      <p className=' text-base font-semibold text-[#fbfbfb]'> {response} </p>
+      </div>
     </div>
+   </div>
   )
 }
 
